088ba91c93cfcd5decfec83076fac57cd24cd777
SK207_Inst <SK207_Inst@K207_Inst.carrollcc.edu>
2016-09-19 20:06:17 -0400

    Asks for name to give a polite greeting

--- PartnersExample/src/edu/cis232/HelloWorld.java
+++ PartnersExample/src/edu/cis232/HelloWorld.java
@@ -1,9 +1,15 @@
 package edu.cis232;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Earth!");
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = keyboard.nextLine();
+		
+		System.out.printf("Hello %s!%n", name);
 		System.out.println("Goodbye Earth!");
 	}
 
